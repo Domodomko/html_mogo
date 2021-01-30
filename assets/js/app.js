22e59469fec56af8280e9bb6a46c27170397845c
Domodom <viablochko@gmail.com>
2021-01-30 18:40:26 +0600

    Some little changes and fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,74 +1,83 @@
 $(function() {
-
-    var header = $("#header"),
-        introH = $("#intro").innerHeight(),
+    
+    var introH = $("#intro").innerHeight(),
+        header = $("#header"),
         scrollOffset = $(window).scrollTop();
-
-
-    /* Fixed Header */
+    
+    
+//  Fixed Header
     checkScroll(scrollOffset);
-
+    
     $(window).on("scroll", function() {
-        scrollOffset = $(this).scrollTop();
-
+        scrollOffset= $(this).scrollTop ();
         checkScroll(scrollOffset);
+        
     });
-
+    
+    
     function checkScroll(scrollOffset) {
-        if( scrollOffset >= introH ) {
-            header.addClass("fixed");
-        } else {
-            header.removeClass("fixed");
+
+        if(scrollOffset >= introH) {
+            header.addClass("fixed")
         }
+        else {
+            header.removeClass("fixed")
+        }
+        
     }
-
-
-
-    /* Smooth scroll */
+    
+//  Smooth Scroll
+    
     $("[data-scroll]").on("click", function(event) {
         event.preventDefault();
-
+        
         var $this = $(this),
-            blockId = $this.data('scroll'),
+            blockId = $(this).data('scroll'),
             blockOffset = $(blockId).offset().top;
-
-        $("#nav a").removeClass("active");
+        
+        $("nav a").removeClass("active");
         $this.addClass("active");
-
+        
         $("html, body").animate({
-            scrollTop:  blockOffset
+            scrollTop: blockOffset
         }, 500);
     });
 
 
+//  Menu nav toggle
 
-    /* Menu nav toggle */
     $("#nav_toggle").on("click", function(event) {
         event.preventDefault();
-
+        
         $(this).toggleClass("active");
         $("#nav").toggleClass("active");
     });
 
-
-
-    /* Collapse */
-    $("[data-collapse]").on("click", function(event) {
-        event.preventDefault();
-
-        var $this = $(this),
-            blockId = $this.data('collapse');
-
-        $this.toggleClass("active");
+    $(window).on("scroll", function() {
+        $("#nav_toggle").removeClass("active");
+        $("#nav").removeClass("active");
     });
 
 
-    /* Slider */
+//  Accordeon
+
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+        var $this = $(this),
+            blockId = $(this).data('collapse');
+        
+        $this.toggleClass("active");
+        $(blockId).slideToggle();
+        
+    })
+
+//  Slider
+    
     $("[data-slider]").slick({
         infinite: true,
         fade: false,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToscroll: 1
     });
-
+    
 });
